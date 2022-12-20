6c1e300a164072e06542fc77dff28dad0a61bb3a
Lara Cunha <lara2003cunha@gmail.com>
2022-12-20 15:07:57 +0000

    example 4

---
+++ refactoring/src/main/java/pt/up/fe/ldts/example4/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private final String name;
+    private final String phone;
+
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+}
--- refactoring/src/main/java/pt/up/fe/ldts/example4/Worker.java
+++ refactoring/src/main/java/pt/up/fe/ldts/example4/Worker.java
@@ -1,45 +1,24 @@
 package pt.up.fe.ldts.example4;
 
-import java.util.Objects;
-
-public class Worker {
-    private final String name;
-    private final String phone;
+public class Worker extends Person {
     private final String username;
     private final String password;
 
     public Worker(String name, String phone, String username, String password) {
-        this.name = name;
-        this.phone = phone;
+        super(name, phone);
         this.username = username;
         this.password = password;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public boolean login(String username, String password) {
-        return this.username.equals(username) && this.password.equals(password);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Worker worker = (Worker) o;
-        return Objects.equals(name, worker.name) &&
-                Objects.equals(phone, worker.phone) &&
-                Objects.equals(username, worker.username) &&
-                Objects.equals(password, worker.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, phone, username, password);
+        return getUsername().equals(username) && getPassword().equals(password);
     }
 }
