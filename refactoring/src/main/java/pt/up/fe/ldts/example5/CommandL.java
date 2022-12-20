e6c2fd0128a42057c894b7479db0e4d24938423b
Lara Cunha <lara2003cunha@gmail.com>
2022-12-20 15:10:37 +0000

    example 5

---
+++ refactoring/src/main/java/pt/up/fe/ldts/example5/CommandL.java
@@ -0,0 +1,16 @@
+public class CommandL extends Command {
+
+    public CommandL(Position p) {
+        super(p);
+    }
+
+    @Override
+    public Position execute() {
+        if (getPosition().getDirection() == 'N') getPosition().setDirection('W');
+        else if (getPosition().getDirection() == 'W') getPosition().setDirection('S');
+        else if (getPosition().getDirection() == 'S') getPosition().setDirection('E');
+        else if (getPosition().getDirection() == 'E') getPosition().setDirection('N');
+        return getPosition();
+    }
+
+}
--- refactoring/src/main/java/pt/up/fe/ldts/example5/Turtle.java
+++ refactoring/src/main/java/pt/up/fe/ldts/example5/Turtle.java
@@ -1,44 +1,47 @@
 package pt.up.fe.ldts.example5;
 
 public class Turtle {
-    private int row;
-    private int column;
-    private char direction;
+    Position p;
 
     public Turtle(int row, int column, char direction) {
-        this.row = row;
-        this.column = column;
-        this.direction = direction;
+        this.p = new Position(row, column, direction);
     }
 
     public int getRow() {
-        return row;
+        return p.getRow();
     }
 
     public int getColumn() {
-        return column;
+        return p.getColumn();
     }
 
     public char getDirection() {
-        return direction;
+        return p.getDirection();
+    }
+
+    public void setRow(int row) {
+        p.setRow(row);
+    }
+
+    public void setColumn(int column) {
+        p.setColumn(column);
+    }
+
+    public void setDirection(char direction) {
+        p.setDirection(direction);
+    }
+
+    public void setP(Position p) {
+        this.p = p;
     }
 
     public void execute(char command) {
         if (command == 'L') { // ROTATE LEFT
-            if (direction == 'N') direction = 'W';
-            else if (direction == 'W') direction = 'S';
-            else if (direction == 'S') direction = 'E';
-            else if (direction == 'E') direction = 'N';
+            setP(new CommandL(p).execute());
         } else if (command == 'R') { // ROTATE RIGHT
-            if (direction == 'N') direction = 'E';
-            else if (direction == 'E') direction = 'S';
-            else if (direction == 'S') direction = 'W';
-            else if (direction == 'W') direction = 'N';
+            setP(new CommandR(p).execute());
         } else if (command == 'F'){ // MOVE FORWARD
-            if (direction == 'N') row--;
-            if (direction == 'S') row++;
-            if (direction == 'W') column--;
-            if (direction == 'E') column++;
+            setP(new CommandF(p).execute());
         }
     }
 }
